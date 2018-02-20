9396de26f43f8ec8c13d10402d2e505e6ec71ba1
Robert Kooima <robert.kooima@gmail.com>
2018-02-20 10:39:18 -0600

    Update GL header for C++11 compat

--- gl.hpp
+++ gl.hpp
@@ -39,6 +39,7 @@
 #endif
 
 #include <cstdlib>
+#include <cstring>
 #include <cstdio>
 #include <cmath>
 
@@ -186,10 +187,10 @@ namespace gl
     {
         vec4 M[4];
 
-        mat4(GLfloat m00=0, GLfloat m01=0, GLfloat m02=0, GLfloat m03=0,
-             GLfloat m10=0, GLfloat m11=0, GLfloat m12=0, GLfloat m13=0,
-             GLfloat m20=0, GLfloat m21=0, GLfloat m22=0, GLfloat m23=0,
-             GLfloat m30=0, GLfloat m31=0, GLfloat m32=0, GLfloat m33=0)
+        mat4(GLfloat m00=1, GLfloat m01=0, GLfloat m02=0, GLfloat m03=0,
+             GLfloat m10=0, GLfloat m11=1, GLfloat m12=0, GLfloat m13=0,
+             GLfloat m20=0, GLfloat m21=0, GLfloat m22=1, GLfloat m23=0,
+             GLfloat m30=0, GLfloat m31=0, GLfloat m32=0, GLfloat m33=1)
         {
             M[0] = vec4(m00, m01, m02, m03);
             M[1] = vec4(m10, m11, m12, m13);
@@ -597,7 +598,15 @@ namespace gl
 
     inline int write_tga(const char *filename, int w, int h, int d, void *p)
     {
-        tga_head head = { 0, 0, 2, 0, 0, 0, 0, 0, w, h, d, (d == 32 ? 8 : 0) };
+        tga_head head;
+
+        memset(&head, 0, sizeof (tga_head));
+
+        head.image_type       = 2;
+        head.image_width      = (unsigned short) w;
+        head.image_height     = (unsigned short) h;
+        head.image_depth      = (unsigned  char) d;
+        head.image_descriptor = (d == 32) ? 8 : 0;
 
         if (d == 24 || d == 32)
         {
@@ -605,7 +614,7 @@ namespace gl
             {
                 if (fwrite(&head, sizeof (tga_head), 1, stream) == 1)
                 {
-                    if (fwrite(p, d / 8, w * h, stream) == w * h)
+                    if (fwrite(p, d / 8, w * h, stream) == size_t(w * h))
                     {
                         fclose(stream);
                         return 0;
@@ -639,7 +648,7 @@ namespace gl
                     {
                         if (void *p = calloc(w * h, d / 8))
                         {
-                            if (fread(p, d / 8, w * h, stream) == w * h)
+                            if (fread(p, d / 8, w * h, stream) == size_t(w * h))
                             {
                                 fclose(stream);
                                 return p;
